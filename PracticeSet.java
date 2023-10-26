7e5bf9f665086046d466fb0a98b8442476b040cf
Arshiyamp61 <143844103+Arshiyamp61@users.noreply.github.com>
2023-10-26 23:46:13 +0530

    Add files via upload

---
+++ PracticeSet.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class PracticeSet {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the First number");
+        int a = sc.nextInt();
+        System.out.println("Enter the Second number");
+        int b = sc.nextInt();
+        System.out.println("Enter the Third number");
+        int c = sc.nextInt();
+
+        int sum =  ( a+ b + c);
+        System.out.println(sum);
+    }
+}
