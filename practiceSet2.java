ba0d04fb124648b3d589460300343345679b62fa
Arshiyamp61 <143844103+Arshiyamp61@users.noreply.github.com>
2023-10-26 23:46:32 +0530

    Add files via upload

---
+++ practiceSet2.java
@@ -0,0 +1,13 @@
+public  class practiceSet2 {
+    public static void main(String args[]) {
+
+        
+        int Subject1 = 45;
+         int Subject2 = 90;
+ int Subject3 = 16;
+float  cgpa = (Subject1 +  Subject2 + Subject3 ) / 30;
+   System.out.println(cgpa);     
+        
+
+    }
+}
