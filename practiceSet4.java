1b7534116bfc7fd98462221b02e7a998eafb473d
Arshiyamp61 <143844103+Arshiyamp61@users.noreply.github.com>
2023-10-31 23:58:12 +0530

    code

---
+++ practiceSet4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class practiceSet4 {
+public static void main (String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the digit ");
+    float digit = sc.nextInt();
+    float   Kilometer = (0.62f * digit);
+    System.out.println(Kilometer);
+
+}
+}
---
+++ practiceSet5.java
@@ -0,0 +1,13 @@
+//Question 
+import java.util.*;
+public class practiceSet5 {
+    public static void main(String args[]) {
+       
+        System.out.println("Enter the number");
+ Scanner sc = new Scanner (System.in);
+ System.out.println(sc.hasNextInt());
+    }
+}
+
+
+//
