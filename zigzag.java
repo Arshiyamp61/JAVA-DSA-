2da4da528b85dc04955de62823e3481f81bb86ea
Arshiyamp61 <143844103+Arshiyamp61@users.noreply.github.com>
2023-11-17 20:47:48 +0530

    zig zag linkedlist solution

---
+++ zigzag.java
@@ -0,0 +1,95 @@
+public class zigzag {
+  public static class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  public static Node head;
+  public static Node tail;
+
+  public void addFirst(int data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+      head = tail = newNode;
+      return;
+    }
+    newNode.next = head;
+    head = newNode;
+  }
+
+  public void addLast(int data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+      head = tail = newNode;
+      return;
+    }
+    tail.next = newNode;
+    tail = newNode;
+  }
+
+  public void print() {
+    Node temp = head;
+    if (head == null) {
+      System.out.println("LL is Empty");
+      return;
+    }
+
+    while (temp != null) {
+      System.out.print(temp.data + " ");
+      temp = temp.next;
+    }
+    System.out.println();
+  }
+
+  // mid
+  public void zig() {
+    Node slow = head;
+    Node fast = head.next;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    Node mid = slow;
+
+    Node curr = mid.next;
+    mid.next = null;
+    Node prev = null;
+    Node next;
+
+    while (curr != null) {
+      next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    Node left = head;
+    Node right = prev;
+    Node nextL, nextR;
+    while (left != null && right != null) {
+      nextL = left.next;
+      left.next = right;
+      nextR = right.next;
+      right.next = nextL;
+
+      left = nextL;
+      right = nextR;
+    }
+  }
+
+  public static void main(String args[]) {
+    zigzag ll = new zigzag();
+    ll.addLast(1);
+    ll.addLast(2);
+    ll.addLast(3);
+    ll.addLast(4);
+    ll.addLast(5);
+
+    ll.zig();
+    ll.print();
+  }
+}
