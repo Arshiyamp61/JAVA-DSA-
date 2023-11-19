3b45279740b7fbedf6babfb66b413dbf580f9294
Arshiyamp61 <143844103+Arshiyamp61@users.noreply.github.com>
2023-11-19 21:16:50 +0530

    Add files via upload

---
+++ revrese.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class revrese{
+  public static String  revreseString(String str  ){
+    Stack<Character>s= new Stack<>();
+    int idx=0;
+    while(idx <str.length()){
+ s.push(str.charAt(idx));
+ idx++;
+    }
+   
+    StringBuilder result = new StringBuilder("");
+    while(!s.isEmpty()){
+      char curr = s.pop();
+      result.append(curr);
+    }
+    return  result.toString();
+  }
+  public static void main (String args[]){
+String str = "abc";
+   String result = revreseString(str);
+   System.out.println(result);
+  }
+}
